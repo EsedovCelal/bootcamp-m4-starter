adc0d0e98290328dda4a99ada4cea2bf906132e0
EsedovCelal <107685956+EsedovCelal@users.noreply.github.com>
2023-01-07 01:10:46 -0800

    added some files and translate to english

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
-import './Favorites.css';
-
+import React, { Component } from "react";
+import "./Favorites.css";
 
 class Favorites extends Component {
-    state = {
-        title: 'Новый список',
-        movies: [
-            { imdbID: 'tt0068646', title: 'The Godfather', year: 1972 }
-        ]
-    }
-    render() { 
-        return (
-            <div className="favorites">
-                <input value="Новый список" className="favorites__name" />
-                <ul className="favorites__list">
-                    {this.state.movies.map((item) => {
-                        return <li key={item.id}>{item.title} ({item.year})</li>;
-                    })}
-                </ul>
-                <button type="button" className="favorites__save">Сохранить список</button>
-            </div>
-        );
-    }
+  state = {
+    title: "Новый список",
+    movies: [{ imdbID: "tt0068646", title: "The Godfather", year: 1972 }],
+  };
+  render() {
+    return (
+      <div className="favorites">
+        <input value="Новый список" className="favorites__name" />
+        <ul className="favorites__list">
+          {this.state.movies.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.title} ({item.year})
+              </li>
+            );
+          })}
+        </ul>
+        <button type="button" className="favorites__save">
+          Save list
+        </button>
+      </div>
+    );
+  }
 }
- 
+
 export default Favorites;
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import './MovieItem.css';
+import React, { Component } from "react";
+import "./MovieItem.css";
 
 class MovieItem extends Component {
-    render() {
-        const { title, year, poster } = this.props;
-        return (
-            <article className="movie-item">
-                <img className="movie-item__poster" src={poster} alt={title} />
-                <div className="movie-item__info">
-                    <h3 className="movie-item__title">{title}&nbsp;({year})</h3>
-                    <button type="button" className="movie-item__add-button">Добавить в список</button>
-                </div>
-            </article>
-        );
-    }
+  render() {
+    const { title, year, poster } = this.props;
+    return (
+      <article className="movie-item">
+        <img className="movie-item__poster" src={poster} alt={title} />
+        <div className="movie-item__info">
+          <h3 className="movie-item__title">
+            {title}&nbsp;({year})
+          </h3>
+          <button type="button" className="movie-item__add-button">
+            Add to list
+          </button>
+        </div>
+      </article>
+    );
+  }
 }
- 
+
 export default MovieItem;
